1a648edd602a0d9359edd745af7754d626010216
MathewIsBack <oseghaleokononfua@gmail.com>
2025-08-21 13:08:30 +0100

    new

---
+++ main.py
@@ -0,0 +1,49 @@
+#this us my first python program
+
+
+#strings
+first_name = "Bro"
+food = "pizza"
+email = "bro123@fakecom"
+
+#Integers
+age = 25
+quantity = 3
+
+#float 
+price = 10.99
+
+#boolean 
+is_student = False
+
+
+#price = int(price)
+#print(price)
+#print(type(price))
+
+# name = input("what is your name?: ")
+# age = input("how old are you: ")
+
+# age = int(age) + 1
+
+# print(f"Hello {name}!")
+# print("Happy Birthday")
+# print(f"you are {age} years old")
+
+# Calc the area of the rectangle
+# length = input("Enter the length: ")
+# width = input("Enter the width: ")
+
+# area = float(length) * float(width)
+
+# print(f"The area is: {area}cm")
+
+# Shopping Cart Program
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: {total}")
