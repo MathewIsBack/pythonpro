62d2593a764ab5921a9dec694f047cf4951f4099
MathewIsBack <oseghaleokononfua@gmail.com>
2025-08-21 13:49:50 +0100

    simple calc

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,33 @@
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
-adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing'")
-adjective3 = input("Enter an adjective (description): ")
+# adjective1 = input("Enter an adjective (description): ")
+# noun1 = input("Enter a noun (person, place, thing): ")
+# adjective2 = input("Enter an adjective (description): ")
+# verb1 = input("Enter a verb ending with 'ing'")
+# adjective3 = input("Enter an adjective (description): ")
 
 
-print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, i saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
+# print(f"Today I went to a {adjective1} zoo.")
+# print(f"In an exhibit, i saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}!")
+
+import math
+# print(math.pi)
+
+radius = float(input('Enter the radius of a circle: '))
+
+circumference = 2 * math.pi * radius
+
+area = math.pi * pow(radius, 2)
+
+print(f"The circumference is: {round(circumference, 2)}cm")
+
+print(f"The area of the circle is: {round(area, 2)}cm^2")
+
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"The Hypotenuse of the triangle is {round(c, 2)}cm^2")
+
+
