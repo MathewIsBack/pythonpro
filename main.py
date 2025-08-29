8dff3f876b83f1e08764a4ef1eb1209dd0206cf0
MathewIsBack <oseghaleokononfua@gmail.com>
2025-08-29 09:20:16 +0100

    random game

--- main.py
+++ main.py
@@ -153,3 +153,29 @@ is_student = False
 
 # for key, value in capitals.items():
 #     print(f"{key} : {value}")
+
+
+
+
+# Random 
+# import random
+
+# low = 1
+# high = 100
+# options = ("rock", "paper", "scissors")
+# cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# random.shuffle(cards)
+
+# print(cards)
+
+
+
+
+
+
+
+
+
+
+
