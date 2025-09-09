39d014f0cab0a794eed374e70de6d6a457c4e5a5
MathewIsBack <oseghaleokononfua@gmail.com>
2025-09-09 22:14:46 +0100

    objects

---
+++ objects.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("Mustang", 2025, "black", False)
+car2 = Car("Lamboghini", 2024, "blue", False)
+car3 = Car("Ferrari", 2023, "red", True)
+
+
+car1.drive()
+car2.stop()
+car3.describe()
