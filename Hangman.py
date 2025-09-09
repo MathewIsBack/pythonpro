9c18b98a691032ef4f2d33959404d4cc745b95e9
MathewIsBack <oseghaleokononfua@gmail.com>
2025-09-09 02:54:22 +0100

    hangman game

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,6 @@
+from wordslist import words
+
 import random
-
-
-words = ("apple","banana","coconut","orange","pineapple")
 
 
 hangman_art = {
@@ -30,21 +29,63 @@ hangman_art = {
 
 
 
-for line in hangman_art[6]:
-    print(line)
+# for line in hangman_art[6]:
+#     print(line)
 
 
 def display_man(wrong_guesses):
-    pass
+    for line in hangman_art[wrong_guesses]:
+        print(line)
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 def main():
-    pass
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letter = set()
+    is_running = True
+
+    while is_running:
+        display_hint(hint)
+        guess = input("Enter a Letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid Input")
+            continue
+
+        if guess in guessed_letter:
+            print(f"{guess} is already guessed")
+            continue
+
+        guessed_letter.add(guess)
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        elif guess not in answer:
+            wrong_guesses += 1
+            if wrong_guesses >= len(hangman_art) - 1:
+                display_man(wrong_guesses)
+                display_answer(answer)
+                print("YOU LOSE!")
+                break
+
+            display_man(wrong_guesses)
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!")
+            is_running = False
+        
+
+
 
 if __name__ == '__main__':
     main()
---
+++ wordslist.py
@@ -0,0 +1,18 @@
+# words = ("apple","banana","coconut","orange","pineapple")
+
+words = (
+    # Fruits
+    "apple", "banana", "coconut", "orange", "pineapple", "grape", "strawberry",
+    "blueberry", "watermelon", "mango", "kiwi", "papaya", "peach", "pear",
+    "plum", "cherry", "lemon", "lime",
+
+    # Animals
+    "elephant", "giraffe", "tiger", "lion", "zebra", "kangaroo", "dolphin",
+    "whale", "penguin", "rabbit", "squirrel", "turtle", "eagle", "shark",
+    "panda", "koala", "hippopotamus", "rhinoceros",
+
+    # Objects
+    "computer", "keyboard", "monitor", "bottle", "guitar", "pencil", "notebook",
+    "backpack", "window", "chair", "table", "lamp", "camera", "phone", "clock",
+    "bicycle", "helmet", "rocket"
+)
