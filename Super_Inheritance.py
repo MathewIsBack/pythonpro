1577dfb54a5005b1fa450933bbece29e397533f0
MathewIsBack <oseghaleokononfua@gmail.com>
2025-09-09 23:56:22 +0100

    super class

---
+++ Super_Inheritance.py
@@ -0,0 +1,41 @@
+from math import pi
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+    
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'} ")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def describe(self):
+        super().describe()
+        print(f"It is a circle with an area of {round(pi * self.radius * self.radius, 2)}cm^2")
+        
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+
+circle = Circle("red", False, 6)
+triangle = Triangle("blue", True, 4, 8)
+
+print(triangle.height)
+
+triangle.describe()
+
+circle.describe()
+
