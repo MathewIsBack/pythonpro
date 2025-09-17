2cb85e9dbe9dbad25fb9f856e280fd97289d46f8
MathewIsBack <oseghaleokononfua@gmail.com>
2025-09-17 07:50:51 +0100

    decorators

---
+++ decorators.py
@@ -0,0 +1,21 @@
+# Decorators
+
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("*You add sprinkles 🎊*")
+        func(*args, **kwargs)
+    return wrapper
+
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("*You add fudge 🍫*")
+        func(*args, **kwargs)
+    return wrapper
+
+
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavour):
+    print(f"Here is your {flavour} ice cream 🍦")
+
+get_ice_cream("chocolate")
