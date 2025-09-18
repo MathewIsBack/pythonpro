ac938da079244c5e91f84a000b23ec5c3f3e94a1
MathewIsBack <oseghaleokononfua@gmail.com>
2025-09-18 10:03:49 +0100

    writing files

--- writing_files.py
+++ writing_files.py
@@ -12,19 +12,39 @@
 
 
 
-import json
+# import json
 
-employee = {
-    "name" : "Eugene",
-    "age" : 30,
-    "job": "Cook"
-}
+# employee = {
+#     "name" : "Eugene",
+#     "age" : 30,
+#     "job": "Cook"
+# }
 
-file_path = "C:/Users/user/Desktop/output.json"
+# file_path = "C:/Users/user/Desktop/output.json"
+
+# try:
+#     with open(file=file_path, mode="w") as file:
+#         json.dump(employee, file, indent=4)
+#         print(f"json file '{file_path}' was created")
+# except FileExistsError:
+#     print("That file already exists!")
+
+
+
+import csv
+
+employees = [["Name","Age","Job"],
+             ["Eugene", 30 ,"Cook"],
+             ["Splint", 26 ,"Unemployed"],
+             ["Andrey", 25 ,"Engineer"]]
+
+file_path = "C:/Users/user/Desktop/output.csv"
 
 try:
     with open(file=file_path, mode="w") as file:
-        json.dump(employee, file, indent=4)
-        print(f"json file '{file_path}' was created")
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"CSV file '{file_path}' was created")
 except FileExistsError:
     print("That file already exists!")
